7a26ea758ab135cfa7b1d10f394c9cd3427fcf5d
Mo-Greene <13blueboy13@daum.net>
2022-07-23 17:25:24 +0900

    Initial commit

---
+++ src/main/java/com/cicd/jenkins0723/Jenkins0723Application.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkins0723;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins0723Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins0723Application.class, args);
+    }
+
+}
